66d4921b30005758ca890c0a3e043dd06c1cfd67
Wino0 <nameless.aro@gmail.com>
2021-12-08 18:36:49 +0300

    Lab8 add

---
+++ Lab8/blog/articles/static/js/fold-post.js
@@ -0,0 +1,17 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const wrappers = document.querySelector(".archive");
+
+  wrappers.addEventListener("click", (e) => {
+   target = e.target;
+   if (target && target.matches(".fold-button")) {
+     if (target.parentElement.classList.contains("folded")) {
+       target.innerHTML = "Свернуть";
+       target.parentElement.classList.remove("folded");
+     } else {
+       console.log(target.parentElement);
+       target.innerHTML = "Развернуть";
+       target.parentElement.classList.add("folded");
+     }
+   }
+ });
+});
